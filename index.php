9b6510320f8ee76a7b95f0bc92b90dc18331f029
Chrono Lai <x941i6c04@gmail.com>
2014-05-20 16:02:51 +0800

    update index.php indent

--- index.php
+++ index.php
@@ -7,16 +7,16 @@ include APP_CORE."Autoload.php";
 
 $router = new Router();
 // IndexController
-$router->add ("/", 							["IndexController", "index"]);
-$router->add ("/about", 					["IndexController", "about"]);
+$router->add ("/", 				["IndexController", "index"]);
+$router->add ("/about", 			["IndexController", "about"]);
 // BlogController
-$router->get ("/blog", 						["BlogController", "index"]);
+$router->get ("/blog", 				["BlogController", "index"]);
 // $router->get ("/blog/(?<page>\d+)", 		["BlogController", "page"]);
 $router->get ("/blog/detail/(?<id>\d+)", 	["BlogController", "detail"]);
-$router->get ("/blog/create", 				["BlogController", "form"]);
-$router->post("/blog/create", 				["BlogController", "create"]);
+$router->get ("/blog/create", 			["BlogController", "form"]);
+$router->post("/blog/create", 			["BlogController", "create"]);
 $router->get ("/blog/modify/(?<id>\d+)", 	["BlogController", "modify"]);
-$router->post("/blog/update", 				["BlogController", "update"]);
+$router->post("/blog/update", 			["BlogController", "update"]);
 $router->get ("/blog/delete/(?<id>\d+)", 	["BlogController", "delete"]);
 // Can use Anonymous Function
 $router->get ("/hello", function(){
